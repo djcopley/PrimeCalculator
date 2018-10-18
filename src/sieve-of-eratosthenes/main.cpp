5049d3075b3c077b30f50177d71bd7d046079d72
Daniel Copley <djcopley@users.noreply.github.com>
2018-10-18 12:48:45 -0700

    added user arguments to sieve of eratosthenes program

--- src/sieve-of-eratosthenes/main.cpp
+++ src/sieve-of-eratosthenes/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstring>
 #include <time.h>
 
 #include "calcprimes.h"
@@ -9,13 +10,30 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    auto start = time(nullptr);
-    auto primes = CalcPrimes(1000000);
-    auto finish = difftime(time(nullptr), start);
-
-    for (auto b : primes.getPrimes()) {
-        cout << b << endl;
+    if (argc < 2) {
+        fprintf(stderr, "Program requires one positional arguments:\n"
+                        "\t(required) 0: range: upper limit\n"
+			"\t(optional) 1: print the primes to the screen (type yes to enable)\n");
+        return -1;
     }
 
-    std::cout << "\n" << primes.primeCount() << " primes found" << " in " << finish << " seconds." << std::endl;
+    // Yes I am aware of the proper ways to parse user input. I'm lazy.
+    unsigned long upperLimit = strtoul(argv[1], nullptr, 10);
+    bool displayPrimes = false;
+
+    if (argc > 2)
+	    displayPrimes = strcmp(argv[2], "yes") == 0;
+
+    auto start = time(nullptr);
+    auto primes = CalcPrimes(upperLimit);
+    auto finish = difftime(time(nullptr), start);
+    
+    if (displayPrimes) {
+        for (auto b : primes.getPrimes()) {
+            cout << b << endl;
+        }
+    }
+
+    std::cout << primes.primeCount() << " primes found between 0 and " << upperLimit << " in " << finish << " seconds."
+	      << std::endl;
 }
